5f95988bbff96d4c0a327bd57d60b2b963401e63
Crispioso <crispin.merriman@gmail.com>
2017-12-12 14:51:13 +0000

    Get repos from Github instead of using event JSON

--- concourse-develop-status/index.js
+++ concourse-develop-status/index.js
@@ -4,6 +4,7 @@ const Firebase = require('firebase');
 const fetch = require("fetch").fetchUrl;
 const isDevelop = process.env.BUILD === "dev";
 const concourseDomain = isDevelop ? "concourse.onsdigital.co.uk" : "172.31.45.62";
+const reposURL = "https://raw.githubusercontent.com/ONSdigital/dp-developer-dashboard-lambdas/master/get-github-repos/repos.json";
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_KEY,
@@ -32,45 +33,62 @@ const getBuilds = (name) => {
     });
 };
 
-exports.handler = (event, context, callback) => {
+const getReposConfig = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Fetching repos: ", reposURL);
+        fetch(reposURL, fetchOptions, (error, _, response) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(JSON.parse(response).repos);
+        });
+    });
+}
+
+exports.handler = (_, context, callback) => {
     if (!Firebase.apps.length) {
         Firebase.initializeApp(firebaseConfig);
     }
 
-
-    const getAllBuilds = event.repos.map(repo_name => {
-        return new Promise((resolve, reject) => {
-            getBuilds(repo_name).then(response => {
-                console.log("Received response for: " + repo_name);
-                const developBuilds = response.filter(job => {
-                    return (
-                        job.name === ("cmd-develop-build")
-                        ||
-                        job.name === ("develop-build")
-                    )
-                }).map(job => {
-                    return {
-                        status: job.finished_build.status,
-                        name: job.name.replace('-build', ''),
-                        date: new Date(job.finished_build.end_time)
-                    }
-                });
-                Firebase.database().ref("develop_build_statuses").child(`${repo_name}`)
-                    .set(developBuilds)
-                    .then(function (developBuilds) {
-                        resolve();
-                    })
-                    .catch(function (error) {
-                        console.log('Firebase error: ', error);
-                        reject(error);
+    getReposConfig().then(repos => {
+        const getAllBuilds = repos.map(repo_name => {
+            return new Promise((resolve, reject) => {
+                getBuilds(repo_name).then(response => {
+                    console.log("Received response for: " + repo_name);
+                    const developBuilds = response.filter(job => {
+                        return (
+                            job.name === ("cmd-develop-build")
+                            ||
+                            job.name === ("develop-build")
+                        )
+                    }).map(job => {
+                        return {
+                            status: job.finished_build.status,
+                            name: job.name.replace('-build', ''),
+                            date: new Date(job.finished_build.end_time)
+                        }
                     });
-            });
-        })
-    });
-
-    Promise.all(getAllBuilds).then(responses => {
-        context.succeed();
+                    Firebase.database().ref("develop_build_statuses").child(`${repo_name}`)
+                        .set(developBuilds)
+                        .then(function (developBuilds) {
+                            resolve();
+                        })
+                        .catch(function (error) {
+                            console.log('Firebase error: ', error);
+                            reject(error);
+                        });
+                });
+            })
+        });
+    
+        Promise.all(getAllBuilds).then(responses => {
+            context.succeed();
+        }).catch(error => {
+            context.fail(error);
+        });
     }).catch(error => {
-        context.fail();
+        console.error("Error fetching repos: ", error);
+        context.fail(error);
     });
 }
