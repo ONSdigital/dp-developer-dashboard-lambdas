46bf4be1323ccab0cd56b0f467afdd941ee4d71b
Crispioso <crispin.merriman@gmail.com>
2018-03-13 16:02:38 +0000

    Add timer to analytics updating to refresh every 10 seconds

--- google-analytics/index.js
+++ google-analytics/index.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 const gaTableID = process.env.GA_TABLE_ID;
 
-const authoriseClient = new Promise((resolve, reject) => {
+const loadCredentials = new Promise((resolve, reject) => {
     if (!authKey) {
         reject("Unable to encrypt auth file because no pass key provided");
         return;
@@ -37,13 +37,49 @@ const authoriseClient = new Promise((resolve, reject) => {
     });
 });
 
+const activeUsersUpdate = (context, options) => {
+    analytics.data.realtime.get(options, (error, response) => {
+        activeUsersUpdateCounter++;
+        let activeUsers = response.data.totalsForAllResults['rt:activeUsers'];
+
+        try {
+            activeUsers = parseInt(activeUsers);
+        } catch (error) {
+            console.error("Error parsing active users to integer");
+            context.fail(error);
+            return;
+        }
+
+        Firebase.database().ref("analytics").child('active_users')
+            .set(activeUsers)
+            .then(() => {
+                if (activeUsersUpdateCounter >= 6) {
+                    context.succeed();
+                    return; 
+                }
+                timer(context, options);
+            })
+            .catch(error => {
+                console.error('Firebase error: ', error);
+                context.fail(error);
+            });       
+    });
+}
+
+const timerDelay = 10000;
+let activeUsersUpdateCounter = 0;
+const timer = (context, options) => {
+    setTimeout(() => {
+        activeUsersUpdate(context, options);
+    }, timerDelay);
+}
 
 exports.handler = (_, context, callback) => {
     if (!Firebase.apps.length) {
         Firebase.initializeApp(firebaseConfig);
     }
 
-    authoriseClient.then(client => {
+    loadCredentials.then(client => {
         client.authorize((error, tokens) => {
             if (error) {
                 context.fail(error);
@@ -55,27 +91,7 @@ exports.handler = (_, context, callback) => {
                 ids: 'ga:' + gaTableID,
                 metrics: 'rt:activeUsers'
             }
-            analytics.data.realtime.get(options, (error, response) => {
-                let activeUsers = response.data.totalsForAllResults['rt:activeUsers'];
-    
-                try {
-                    activeUsers = parseInt(activeUsers);
-                } catch (error) {
-                    console.error("Error parsing active users to integer");
-                    context.fail(error);
-                    return;
-                }
-    
-                Firebase.database().ref("analytics").child('active_users')
-                    .set(activeUsers)
-                    .then(() => {
-                        context.succeed();
-                    })
-                    .catch(error => {
-                        console.error('Firebase error: ', error);
-                        context.fail(error);
-                    });            
-            });
+            timer(context, options);
         });
     }).catch(error => {
         context.fail(error);
